2f36a5d3933b8ae59c9e33e9c004040ad32499d1
Rithin Skaria <rithin@rithin.net>
2023-12-09 12:47:08 +0300

    Update jumpbox.sh

--- 000-Code files/AppGateway/jumpbox.sh
+++ 000-Code files/AppGateway/jumpbox.sh
@@ -13,7 +13,7 @@ printenv | grep VAR
 echo "Setting up green VM"
 sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
-sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/AppGateway/sample.html" > /var/www/html/index.html
+sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-az500/main/000-Code%20files/AppGateway/sample.html" > /var/www/html/index.html
 sed -i "s/PAGECOLOR/green/g" /var/www/html/index.html
 sed -i "s/VMID/$k/g" /var/www/html/index.html
 exit
@@ -33,7 +33,7 @@ echo "Setting up red VM"
 sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
 sudo mkdir -v /var/www/html/red/
-sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/AppGateway/sample.html" > /var/www/html/index.html
+sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-az500/main/000-Code%20files/AppGateway/sample.html" > /var/www/html/index.html
 sed -i "s/PAGECOLOR/red/g" /var/www/html/index.html
 sed -i "s/VMID/$k/g" /var/www/html/index.html
 cat /var/www/html/index.html > /var/www/html/red/red.html
@@ -55,7 +55,7 @@ echo "Setting up blue VM"
 sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
 sudo mkdir -v /var/www/html/blue/
-sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/AppGateway/sample.html" > /var/www/html/index.html
+sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-az500/main/000-Code%20files/AppGateway/sample.html" > /var/www/html/index.html
 sed -i "s/PAGECOLOR/blue/g" /var/www/html/index.html
 sed -i "s/VMID/$k/g" /var/www/html/index.html
 cat /var/www/html/index.html > /var/www/html/blue/blue.html
